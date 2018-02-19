faa89a3aaad7b88350da0cb1027b079787632399
chance pinkerton <cpink.js@gmail.com>
2018-02-18 17:39:21 -0700

    LocalDb set up with Models And Data folder set up

---
+++ angularcore/Data/CoreCMSApi.cs
@@ -0,0 +1,21 @@
+﻿using angularcore.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace angularcore.Data
+{
+    public class CoreCMSApi : DbContext
+    {
+        public CoreCMSApi(DbContextOptions<CoreCMSApi> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Page> Pages { get; set; }
+        public DbSet<Sidebar> Sidebar { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
